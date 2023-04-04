82fac1180b0cd2a0b839c9db3d84142587020d0c
MohammedHosam <hosamfrw33@gmail.com>
2023-04-04 04:17:36 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# singl-post-defult" 
---
+++ js/main.js
@@ -0,0 +1,68 @@
+// start function1 onmouseover
+let div1Img1H4 = document.getElementById("div1-img1-h4");
+let div11H311b11H3 = document.getElementById("div11-h311-p11-h3"); 
+function myFunction1() {
+    div1Img1H4.style.opacity = 1;
+    div11H311b11H3.style.color = "#fe4a55";
+}
+// start function2 onmouseout
+function myFunction2() {
+    div1Img1H4.style.opacity = 0;
+    div11H311b11H3.style.color = "black";
+}
+
+
+// start function3 onmouseover 
+let div12Img12H42 = document.getElementById("div12-img12-h42");
+let div22H322B22H3 = document.getElementById("div22-h322-p22-h3")
+function myFunction3() {
+    div12Img12H42.style.opacity = 1;
+    div22H322B22H3.style.color = "#fe4a55";
+}
+// start function4 onmouseout
+function myFunction4() {
+    div12Img12H42.style.opacity = 0;
+    div22H322B22H3.style.color = "black";
+}
+
+
+// start function5 onclick
+let input1 = document.getElementById("input1");
+function myFunction5() {
+    input1.value = "";
+}
+
+
+// start function6 onclick
+let input2 = document.getElementById("input2");
+function myFunction6() {
+    input2.value = "";
+}
+
+
+// start function7 onclick
+let input3 = document.getElementById("input3");
+function myFunction7() {
+    input3.value = "";
+}
+
+
+// start function8 onclick
+let input4 = document.getElementById("input4");
+function myFunction8() {
+    input4.value = "";
+}
+
+
+// start function9 onclick
+let textarea1 = document.getElementById("textarea1");
+function myFunction9() {
+    textarea1.placeholder = "";
+}
+
+
+// start function8 onclick
+let input5 = document.getElementById("input5");
+function myFunction10() {
+    input5.value = "";
+}
